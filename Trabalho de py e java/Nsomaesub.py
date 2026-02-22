9326d90ba885451f47f12cae224bfd1fd0950cc4
GeymisonDT <geymisonduarte83@gmail.com>
2026-02-22 16:09:03 -0300

    Add files via upload
    apenas aprendendo o básico em java kkk

---
+++ Trabalho de py e java/Nsomaesub.py
@@ -0,0 +1,11 @@
+N1=int(input("Digite seu 1° número bb:  "))
+N2=int(input("Digite seu 2° número bb: "))
+if N1>N2:
+    Sub=N1-N2
+    print(Sub)
+elif N2>N1:
+    Soma=N2+N1
+    print(Soma)
+else:
+    print("Digite números diferentes,seu porra")
+
