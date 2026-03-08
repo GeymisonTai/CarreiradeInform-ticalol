0f735fbefc9b1b76cc73f8f112dbe998ecb78c68
GeymisonDT <geymisonduarte83@gmail.com>
2026-03-08 20:08:14 -0300

    Delete JavaRepeticion directory

--- JavaRepeticion/1a100de6.py
+++
@@ -1,3 +0,0 @@
-N1=int(input("Digite seu número: "))
-for i in range(N1+101):
-    print(i)
--- JavaRepeticion/ElevaçãodoSegundo.py
+++
@@ -1,4 +0,0 @@
-N1=int(input("Digite seu número (padrão): "))
-N2=int(input("Digite seu número(expoente): "))
-print(f"{N1}^{N2}:{N1**N2}")
-
