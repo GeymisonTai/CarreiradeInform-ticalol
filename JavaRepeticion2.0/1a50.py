9899c8f47ca44bafa98c1d530e58ccdbfc116f1e
GeymisonDT <geymisonduarte83@gmail.com>
2026-03-13 10:03:06 -0300

    Delete JavaRepeticion2.0 directory

--- JavaRepeticion2.0/1a50.py
+++
@@ -1,4 +0,0 @@
-soma=0
-for i in range(1,50+1,):
- soma += i
-print(f"Soma de 1 a 50 é: {soma}")
--- JavaRepeticion2.0/Senha.py
+++
@@ -1,7 +0,0 @@
-senhacorreta="Geymelson"
-senha=input("Digite a senha: ")
-while senha !=senhacorreta:
- print("Senha incorreta!")
- senha=input("Digite a senha: ")
-
-print("Bem vindo Usuario(a)!")
