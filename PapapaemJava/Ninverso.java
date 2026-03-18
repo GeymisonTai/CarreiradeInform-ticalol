aa5564091484dafa061bcb01e1bb0f7f974b607a
GeymisonDT <geymisonduarte83@gmail.com>
2026-03-18 08:05:09 -0300

    Ninverso.java
    Fix the loop condition and correct input method.

--- PapapaemJava/Ninverso.java
+++ PapapaemJava/Ninverso.java
@@ -6,7 +6,7 @@ public class Ninverso {
     System.out.print("Digite seu número(vai ser invertido): ");
    int N1=scanner.nextint();
    int inverso=0;
-   while(N1 != 0){
+   while(N1 > 0){
 int digitado=N1 % 10;
 inverso=inverso * 10 + digitado;
 N1=N1/=10;
