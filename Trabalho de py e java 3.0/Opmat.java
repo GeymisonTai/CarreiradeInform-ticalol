8dc7217bbe070924cf6a511bf822121e26020f35
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-03 17:23:44 -0300

    Opmat.java

--- Trabalho de py e java 3.0/Opmat.java
+++ Trabalho de py e java 3.0/Opmat.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Opmatemáticas {
+public class Opmat{
     public static void main(String[]args){
     Scanner scanner=new Scanner(System.in);
     System.out.print("Digite seu número: ");
