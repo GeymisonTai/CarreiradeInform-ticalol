014a6ce479341fd34b85f99254b80148b0b77b77
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-03 18:40:06 -0300

    Bomdia.java

--- PapapaemJava/Bomdia.java
+++ PapapaemJava/Bomdia.java
@@ -1,6 +1,12 @@
-import java.util.Scanner;
 public class Bomdia{
-public static void main(String[]args){
-for(int i=1;i < 10+1;i++){System.out.println("Bom dia :D");}
 }
+public static void repeticao(){
+    for(int i=1;i <11;i++){
+    System.out.println("Bom dia");
+    }
+
+ }
+
+public static void main(String[] args) {
+     repeticao();
 }
