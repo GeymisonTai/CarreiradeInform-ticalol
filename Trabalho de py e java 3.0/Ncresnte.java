158e8ff45ea39d1af54d7789a7ff2593b1ba29b7
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-03 17:19:55 -0300

    Ncresnte.java

--- Trabalho de py e java 3.0/Ncresnte.java
+++ Trabalho de py e java 3.0/Ncresnte.java
@@ -9,17 +9,16 @@ public class Ncresnte {
      System.out.print("Digite seu número: ");
      int N3=scanner.nextInt();
      int Maior,Menor;
-//maior uau
 if(N1 >= N2 && N1 >= N3){Maior=N1;}
 else if (N2 >= N1 && N2 >= N3){Maior=N2;}
 else{Maior=N3;}
-//menor .-.
+        
 if(N1 <= N2 && N1 <= N3){Menor=N1;}
-else if (N2 <= N1 && N2 <= N3){Menor=N2;}
+else if (N2 <= N1 && N2 <= N3){Menor=N2;
+                               
 else{Menor=N3;}
-//merediano kk
 int Medio=N1+N2+N3-Maior-Menor;
-//bagulho chato pqp,pelo menos esssa questão
+        
 System.out.println(Menor);
 System.out.println(Medio);
 System.out.println(Maior);
