78c240441fe937a84643965dfcb6403e476984d5
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-03 18:44:43 -0300

    Buenosdias.java
    Add Buenosdias class with a method to print 'Bom dia' ten times.

---
+++ PapapaemJava/Buenosdias.java
@@ -0,0 +1,12 @@
+public class Buenosdias{
+}
+public static void repeticao(){
+    for(int i=1;i <11;i++){
+    System.out.println("Bom dia ");
+    }
+
+ }
+
+public static void main(String[] args) {
+     repeticao();
+}
