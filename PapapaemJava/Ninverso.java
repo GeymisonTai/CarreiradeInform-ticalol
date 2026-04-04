8fcf37901cc3fdb74896cc23f6ad43297896cf92
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-04 12:28:15 -0300

    Ninverso.java

--- PapapaemJava/Ninverso.java
+++ PapapaemJava/Ninverso.java
@@ -1,20 +1,31 @@
 import java.util.Scanner;
 
 public class Ninverso {
-  public static void main(String[]args){
-  Scanner scanner=new Scanner(System.in);
-    System.out.print("Digite seu número(vai ser invertido): ");
-   int N1=scanner.nextint();
-   int inverso=0;
-   while(N1 > 0){
-int digitado=N1 % 10;
-inverso=inverso * 10 + digitado;
-N1=N1/=10;
+private int numero;
+public Ninverso(int numero){
+  this.numero = numero;}
+public int inverter(){
+    int n = this.numero;
+    int inverso = 0;
 
-scanner.close();
+    while (n > 0) {int digito = n % 10;inverso = inverso * 10 + digito;n /= 10; }
+return inverso;
+  }
 
-   }
-System.out.println(inverso);
+  public String Resultado(){
+    return "Número invertido: " + inverter();
+  }
 
-    }
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Digite seu número (vai ser invertido): ");
+    int valor = scanner.nextInt();
+
+    Ninverso obj = new Ninverso(valor);
+
+    System.out.println(obj.Resultado());
+
+    scanner.close();
+  }
 }
