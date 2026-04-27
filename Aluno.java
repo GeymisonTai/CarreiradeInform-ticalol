57f49fb74879bece2848024bf71534a441e3db73
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-27 16:35:24 -0300

    Add files via upload

--- Aluno.java
+++ Aluno.java
@@ -1,36 +1,11 @@
-import java.time.LocalDate;
-import java.time.LocalTime;
-public class Aluno {
-  private String responsavel;
-  private String nome;
-  private int mtr;
-  private long telefone;
-  private LocalDate datanas;
-public Aluno(String nome,int mtr,String responsavel,long telefone,LocalDate datanas){
-   this.nome=nome;
-   this.mtr=mtr;
-   this.responsavel=responsavel;
-   this.telefone=telefone;
-   this.datanas=datanas;
-}
-public String Apresentacao(){
-    return "Olá sou "+ nome +" minha matricula é "+ mtr + " minha responsavel é a "+ responsavel +
-    "meu n de telefone é "+ telefone+" data de nascimento "+datanas;
-}
- public static String Horario(){
-   LocalTime agora=LocalTime.now();
-   if (agora.getHour() > 5 && agora.getHour() < 12) {return "Bom dia,";} 
-   else if (agora.getHour() > 12 && agora.getHour() < 18) {return("Boa tarde,");}
-   else{return("Boa noite,");}
-  }
-public String Saudacao(String nome){  
-    return Horario() + this.nome;
-}
-  public static void main(String[]args){
-     Aluno andre=new Aluno ("Geymison",538317,"Maria do Socorro",85992341234L,
-      LocalDate.of(2007, 12, 28));
-     System.out.println(andre.Apresentacao());
-     System.out.println(andre.Saudacao("Geymison"));
-}
-}
+public class Aluno extends Pessoa{
+private int mtr;
+ public Aluno(String nome,int cpf,long telefone,int mtr){
+    this.mtr=mtr;
+     super(nome,cpf,telefone,mtr);
+ }
+ public String Aprender(){
+return "Eu quero aprender, crescer e descobrir até onde sou capaz de chegar.E minha matricula é "+matricula;
 
+ }
+}
