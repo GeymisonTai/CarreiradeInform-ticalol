83e92c48a8f89a79635e7950fb467c255d98c05e
GeymisonDT <geymisonduarte83@gmail.com>
2026-04-29 07:51:51 -0300

    Refactor Aluno class with new structure and methods
    Refactor Aluno class to include new attributes and methods.

--- Aluno.java
+++ Aluno.java
@@ -1,11 +1,30 @@
-public class Aluno extends Pessoa{
-private int mtr;
- public Aluno(String nome,int cpf,long telefone,int mtr){
-    this.mtr=mtr;
-     super(nome,cpf,telefone,mtr);
- }
- public String Aprender(){
-return "Eu quero aprender, crescer e descobrir até onde sou capaz de chegar.E minha matricula é "+matricula;
-
- }
+import java.time.LocalTime;
+public class Aluno{
+    private String responsavel;
+    private String nome;
+    private int mtr;
+    private int telefone;
+    public Aluno(String nome,int mtr,String responsavel,int telefone){ 
+        this.nome=nome;
+        this.mtr=mtr;
+        this.responsavel=responsavel;
+        this.telefone=telefone;
+    }
+    
+    public String Apresentaco(){
+        return "Olá sou "+ nome + " minha responsável é a " + responsavel + " minha matricula é " + mtr+".Meu número é " +telefone + " do ano de ";
+        }
+    public int Horario(){
+     int agora=LocalTime.now().getHour();
+     if(5 <= agora >= 11){System.out.println("Bom dia");}
 }
+        public String Saudacao(String nome){
+            return Horario() + nome;
+            }
+
+            public static void main(String[] args) {
+    Aluno andre=new Aluno("Geymison",538317,"Maria do Socorro",99855857);
+    System.out.println(andre.Apresentaco());
+    System.out.println(andre.Saudacao("Geymison"));
+}
+ }
