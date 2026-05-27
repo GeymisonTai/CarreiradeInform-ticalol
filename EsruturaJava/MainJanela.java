e0698089b29db14b1b21b1cbb1a71ca85f805a7c
GeymisonDT <geymisonduarte83@gmail.com>
2026-05-26 21:34:31 -0300

    Update MainJanela.java

--- EsruturaJava/MainJanela.java
+++ EsruturaJava/MainJanela.java
@@ -1,16 +1,54 @@
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextArea;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-import java.awt.Color;
-import java.awt.Font;
 public class MainJanela {
-    public static void main (String[]args){
-
-    
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Cadastro");
+        frame.setSize(400, 250);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(5, 2, 10, 10));
+        
+        JLabel labelNome = new JLabel("Nome:");
+        JTextField tfNome = new JTextField();
+        
+        JLabel labelCPF = new JLabel("CPF:");
+        JTextField tfCPF = new JTextField();
+        
+        JLabel labelTelefone = new JLabel("Telefone:");
+        JTextField tfTelefone = new JTextField();
+        
+        JButton btnLimpar = new JButton("Limpar");
+        JButton btnEnviar = new JButton("Enviar");
+        
+        panel.add(labelNome);
+        panel.add(tfNome);
+        panel.add(labelCPF);
+        panel.add(tfCPF);
+        panel.add(labelTelefone);
+        panel.add(tfTelefone);
+        panel.add(btnLimpar);
+        panel.add(btnEnviar);
+        
+        btnEnviar.addActionListener(e -> {
+            System.out.println("Nome: " + tfNome.getText());
+            System.out.println("CPF: " + tfCPF.getText());
+            System.out.println("Telefone: " + tfTelefone.getText());
+        });
+        
+        btnLimpar.addActionListener(e -> {
+            tfNome.setText("");
+            tfCPF.setText("");
+            tfTelefone.setText("");
+        });
+        
+        frame.add(panel);
+        frame.setVisible(true);
+    }
 }
- }
-
