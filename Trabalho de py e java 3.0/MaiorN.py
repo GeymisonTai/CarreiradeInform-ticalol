a163b9a8f3f4e69831bf524c798771a5a8a3021e
GeymisonDT <geymisonduarte83@gmail.com>
2026-06-02 20:40:32 -0300

    Delete Trabalho de py e java 3.0 directory

--- Trabalho de py e java 3.0/MaiorN.py
+++
@@ -1,8 +0,0 @@
-N1=int(input("Digite seu número: "))
-N2=int(input("Digite seu número: "))
-if N1 > N2:
-    print(f"{N1} é maior")
-elif N2 >N1 :
-    print(f"{N2} é maior")
-else:
-    print("Número igual,coloque n diferentes.")
--- Trabalho de py e java 3.0/Soma.py
+++
@@ -1,4 +0,0 @@
-N1=int(input("Digite o número bb: "))
-N2=int(input("Digite o número bb: "))
-Soma=N1 + N2
-print(Soma)
